4c44f9c91c84a942b4e34b4a40600dc0d3525bc0
Gregory Chen <gregorychen3@gmail.com>
2022-08-09 00:37:25 -0400

    Form

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,47 +1,19 @@
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Paper from "@mui/material/Paper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import Stepper from "@mui/material/Stepper";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Form from "./components/Form";
 
 const theme = createTheme();
-const steps = ["Setup", "Upload contacts", "Confirm", "Send"];
-
-function getStepContent(step: number) {
-  switch (step) {
-    case 0:
-      return "s1";
-    case 1:
-      return "s2";
-    case 2:
-      return "s3";
-    default:
-      throw new Error("Unknown step");
-  }
-}
 
 export default function App() {
-  const [activeStep, setActiveStep] = useState(0);
-
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep(activeStep - 1);
-  };
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
       <AppBar
         position="absolute"
         color="default"
@@ -57,51 +29,9 @@ export default function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper
-          variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-        >
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          <>
-            {activeStep === steps.length ? (
-              <>
-                <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
-                </Typography>
-                <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
-                </Typography>
-              </>
-            ) : (
-              <>
-                {getStepContent(activeStep)}
-                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                  {activeStep !== 0 && (
-                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                      Back
-                    </Button>
-                  )}
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    sx={{ mt: 3, ml: 1 }}
-                  >
-                    {activeStep === steps.length - 1 ? "Place order" : "Next"}
-                  </Button>
-                </Box>
-              </>
-            )}
-          </>
-        </Paper>
+        <Form />
       </Container>
 
       <MemoryRouter>
