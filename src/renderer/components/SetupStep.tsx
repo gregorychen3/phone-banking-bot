202e68eeb69dc59476e854fbeacd438d13424cf0
Gregory Chen <gregorychen3@gmail.com>
2022-08-09 02:05:39 -0400

    shrink all labels

--- src/renderer/components/SetupStep.tsx
+++ src/renderer/components/SetupStep.tsx
@@ -12,6 +12,7 @@ export function SetupStep() {
           label="Sender Name"
           type="text"
           fullWidth
+          InputLabelProps={{ shrink: true }}
         />
       </Grid>
       <Grid item xs={12}>
@@ -20,8 +21,10 @@ export function SetupStep() {
           name="messageTemplate"
           label="Text Message Template"
           type="text"
+          helperText="Use SENDER_NAME and RECIPIENT_NAME placeholders"
           multiline
           fullWidth
+          InputLabelProps={{ shrink: true }}
         />
       </Grid>
     </Grid>
--- src/renderer/components/UploadStep.tsx
+++ src/renderer/components/UploadStep.tsx
@@ -14,6 +14,7 @@ export function UploadStep() {
           type="text"
           multiline
           fullWidth
+          InputLabelProps
         />
       </Grid>
     </Grid>
