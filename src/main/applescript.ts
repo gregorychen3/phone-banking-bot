6c5bc200348451313afc67e296f9f4b6d5905a11
Gregory Chen <gregorychen3@gmail.com>
2025-06-19 15:40:12 -0400

    backend

---
+++ src/main/applescript.ts
@@ -0,0 +1,33 @@
+import { Contact } from 'types';
+
+export const getAppleScript = (
+  senderName: string,
+  messageTemplate: string,
+  contacts: Contact[],
+) =>
+  `tell application "Messages"
+  set smsService to 1st service whose service type = SMS
+  ${contacts
+    .map((c) => {
+      const number = removeNonNumericChars(c.number);
+      const msg = renderMessage(messageTemplate, senderName, c.name);
+      return `set recipient to buddy "${number}" of smsService
+              send "${msg}" to recipient`;
+    })
+    .join('\n')}
+  end tell`;
+
+const renderMessage = (
+  messageTemplate: string,
+  senderName: string,
+  recipientName: string,
+) => escapeStr(messageTemplate.replaceAll('RECIPIENT_NAME', recipientName));
+
+/**
+ * \, ", and ' have special meaning in Applescript and need to be escaped. See:
+ * https://developer.apple.com/library/archive/documentation/AppleScript/Conceptual/AppleScriptLangGuide/reference/ASLR_classes.html#//apple_ref/doc/uid/TP40000983-CH1g-DontLinkElementID_57
+ */
+const escapeStr = (s: string) =>
+  s.replaceAll(/[\\"]/g, '\\$&').replaceAll(`'`, `'"'"'`);
+
+const removeNonNumericChars = (s: string) => s.replace(/\D/g, '');
--- src/main/main.ts
+++ src/main/main.ts
@@ -8,10 +8,13 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import { exec } from 'child_process';
+import { app, BrowserWindow, ipcMain, shell } from 'electron';
 import log from 'electron-log';
+import { autoUpdater } from 'electron-updater';
+import path from 'path';
+import { ExecResult, SendTextsChannelRequest } from '../types';
+import { getAppleScript } from './applescript';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
@@ -29,6 +32,20 @@ ipcMain.on('ipc-example', async (event, arg) => {
   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
   console.log(msgTemplate(arg));
   event.reply('ipc-example', msgTemplate('pong'));
+});
+
+ipcMain.on('send-texts', async (event, args: SendTextsChannelRequest) => {
+  const { senderName, messageTemplate, contacts } = args[0];
+
+  try {
+    const script = getAppleScript(senderName, messageTemplate, contacts);
+    const { stdout, stderr } = await exec(`osascript <<< '${script}'`);
+    const successRes: ExecResult = { stdout, stderr };
+    event.reply('send-texts', successRes);
+  } catch (e: any) {
+    const errorRes: ExecResult = { error: `${e}` };
+    event.reply('send-texts', errorRes);
+  }
 });
 
 if (process.env.NODE_ENV === 'production') {
