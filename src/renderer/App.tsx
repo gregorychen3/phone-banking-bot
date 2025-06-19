9c733feb6b9fea9c4bc28bdecbe12f4239a6a83f
Gregory Chen <gregorychen3@gmail.com>
2025-06-19 15:43:26 -0400

    install mui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,10 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function Hello() {
   return (
