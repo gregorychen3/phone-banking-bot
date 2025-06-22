ff55c67b8cc26d7b8ce22572e4e90fba5e4a8312
Gregory Chen <gregorychen3@gmail.com>
2025-06-21 23:49:58 -0400

    err type

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,7 @@ export type IpcChannels = "ipc-example" | "send-texts" | "save-file";
 export type File = { name: string; data: ArrayBuffer };
 
 export type SaveFileRequest = [{ file: File }];
-export type SaveFileResponse = { error: string } | { filePath: string };
+export type SaveFileResponse = { filePath: string } | ErrorResponse;
 
 export type SendTextsChannelRequest = [
   { senderName: string; messageTemplate: string; contacts: Contact[] },
@@ -17,6 +17,12 @@ export type SendTextsChannelRequest = [
 export type ErrorExecResult = { error?: string };
 export type SuccessExecResult = { stderr: string; stdout: string };
 export type ExecResult = ErrorExecResult | SuccessExecResult;
+
+export type ErrorResponse = { error: string };
+
+export function isErrorResponse(x: any): x is ErrorResponse {
+  return !!x.error;
+}
 
 export function isErrorExecResult(x: ExecResult): x is ErrorExecResult {
   return x.hasOwnProperty("error");
