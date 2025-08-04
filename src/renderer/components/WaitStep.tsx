7ca333b0b6a717f1134316f4e617869b4b873452
Gregory Chen <gregorychen3@gmail.com>
2025-08-03 23:53:56 -0400

    wait step partial impl

--- src/renderer/components/WaitStep.tsx
+++ src/renderer/components/WaitStep.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Grid, LinearProgress } from "@mui/material";
+import { Alert, Box, LinearProgress, Stack } from "@mui/material";
 import { formatDuration } from "date-fns";
 import { useSelector } from "react-redux";
 import { selectContacts } from "../redux/formSlice";
@@ -9,20 +9,18 @@ export function WaitStep() {
   const numContacts = useSelector(selectContacts).length;
 
   return (
-    <Grid container spacing={4}>
-      <Grid size={{ xs: 12 }}>
-        <LinearProgress />
-      </Grid>
+    <Stack spacing={4}>
+      <LinearProgress />
 
-      <Grid size={{ xs: 12 }}>
+      <div>
         Your texts are being sent with a{" "}
         <Box fontWeight="bold" component="span">
           5 second delay
         </Box>{" "}
         between each to avoid triggering your carrier's rate limits.
-      </Grid>
+      </div>
 
-      <Grid size={{ xs: 12 }}>
+      <div>
         Since you have{" "}
         <Box color="success.main" component="span">
           {numContacts}
@@ -32,13 +30,11 @@ export function WaitStep() {
           {formatDuration({ seconds: numContacts * 5 })}
         </Box>
         . Take a coffee break! ☕️
-      </Grid>
+      </div>
 
-      <Grid size={{ xs: 12 }}>
-        <Alert severity="warning">
-          Don't close this window! We'll let you know when we're done.
-        </Alert>
-      </Grid>
-    </Grid>
+      <Alert severity="warning">
+        Don't close this window! We'll let you know when we're done.
+      </Alert>
+    </Stack>
   );
 }
